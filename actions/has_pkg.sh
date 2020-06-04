e805701451e5c70749a2fbda2df6b2706f75124a
Salman Waheed <mkdirenv@gmail.com>
2020-06-04 14:53:10 +0400

    bug fixees

--- actions/has_pkg.sh
+++ actions/has_pkg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 has_pkg() {
-  command -v "$@"
+  command -v "$@" >/dev/null 2>&1
 }
--- actions/ins_pkg_info.sh
+++ actions/ins_pkg_info.sh
@@ -3,7 +3,7 @@
 ins_pkg_with_tiny_info() {
   NAME=$1
   SIZE=$(sudo apt-cache --no-all-versions show "$NAME" | grep -i '^Installed-Size: ' | awk '{print $2}')
-  sleep 2
+  sleep 1
   echo "installing $NAME... Size: $(( $SIZE / 1024 ))MB. It may take time to download and install."
   sudo apt install -y "$NAME" >/dev/null 2>&1 && echo "$NAME has installed!"
 }
